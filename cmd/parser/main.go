b3246d1d13f3452d3c23f854f73829be66824e63
UshakovN <UshakovNikA@mpei.ru>
2022-06-16 03:01:04 +0300

    add primary parser

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,7 +1,8 @@
 package main
 
+import "github.com/UshakovN/practice/internal/app/parser"
+
 func main() {
 	tags := []string{"IIAM0WMR/invitrogen", "IIAM3NW4/gibco"}
-	// parser.FisherSciencific(tags[1])
-	// "github.com/UshakovN/practice/internal/parser"
+	parser.FisherSciencific(tags[1])
 }
--- internal/app/parser/parser.go
+++ internal/app/parser/parser.go
@@ -2,17 +2,49 @@ package parser
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
+	"regexp"
 
-	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
+func findCurrentLink(child *html.Node) {
+	reg, err := regexp.Compile(`/shop/.+[^=]$`)
+	if err != nil {
+		log.Fatalf("invalid regex: %s", err)
+	}
+	if child.Type == html.ElementNode &&
+		child.Data == "a" {
+		for _, childAttr := range child.Attr {
+			if childAttr.Key == "href" && reg.MatchString(childAttr.Val) {
+				fmt.Println(childAttr.Val)
+			}
+		}
+	}
+	for c := child.FirstChild; c != nil; c = c.NextSibling {
+		findCurrentLink(c)
+	}
+}
+
+func getItemLinks(node *html.Node) {
+	if node.Type == html.ElementNode && node.Data == "div" {
+		for _, attr := range node.Attr {
+			if attr.Key == "class" &&
+				attr.Val == "row search_result_item displayToggleControlled " {
+				findCurrentLink(node)
+
+			}
+		}
+	}
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		getItemLinks(c)
+	}
+}
+
 func FisherSciencific(brandTag string) {
-	URL := fmt.Sprintf("https://www.fishersci.com/us/en/brands/%s.html", brandTag)
-	resp, err := http.Get(URL)
+	url := fmt.Sprintf("https://www.fishersci.com/us/en/brands/%s.html", brandTag)
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("uncorrect ulr: %s", err)
 	}
@@ -20,11 +52,21 @@ func FisherSciencific(brandTag string) {
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf("error status code: %d %s", resp.StatusCode, resp.Status)
 	}
-	_, err = goquery.NewDocumentFromReader(resp.Body)
+	/*
+		_, err = goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Fatalf("unexpected error %s", err)
+		}
+		file, err := os.Create("buffer.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		io.Copy(file, resp.Body)
+	*/
+	doc, err := html.Parse(resp.Body)
 	if err != nil {
 		log.Fatalf("unexpected error %s", err)
 	}
-	file, _ := os.Create("buffer.html")
-	defer file.Close()
-	io.Copy(file, resp.Body)
+	getItemLinks(doc)
 }
